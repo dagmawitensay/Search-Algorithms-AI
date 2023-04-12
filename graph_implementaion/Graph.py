46599da7aaa5222e6e0cc9e02ac354cf0e0d7791
dagiTensay <dagitensay@gmail.com>
2023-04-13 00:13:53 +0300

    add locations attribute

--- graph_implementaion/Graph.py
+++ graph_implementaion/Graph.py
@@ -1,20 +1,26 @@
-
 class Graph:
     """
     Graph class that defines a graph data structure
     """
     def __init__(self):
         self.graph = {}
-    
+        self.locations = {}
+
     def createNode(self, node):
         """
         Create a node in the graph
+        Args:
+            node: value of node of creation
         """
         self.graph[node] = []
     
     def insertEdge(self, node_A, node_B, cost):
         """
         Insert Edge between node_A and node_B
+        Args:
+            node_A: first node
+            node_B: second node
+            cost: cost between node_A and node_B
         """
 
         if not node_A in self.graph:
@@ -28,6 +34,10 @@ class Graph:
     def deleteEdge(self, node_A, node_B, cost):
         """
         delete edge between node node_A and node_B
+        Args:
+            node_A: first node
+            node_B: second node
+            cost: cost between node_A and node_B
         """
         self.graph[node_A].remove((node_B, cost))
         self.graph[node_B].remove((node_A, cost))
@@ -42,6 +52,3 @@ class Graph:
                     self.graph[node].remove(neighbor)
         
         del self.graph[node_A]
-
-
-
